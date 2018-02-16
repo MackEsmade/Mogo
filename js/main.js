535f31476e49e6aa1788ffab85533e12dcd4b065
MackEsmade <mac.esmade@gmail.com>
2018-02-16 15:45:01 +0800

    Mogo Upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    });
+
